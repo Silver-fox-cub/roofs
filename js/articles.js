f777868156f27f637a5c6cd8ebff3217b1a489dd
Silver-fox-cub <40055846+Silver-fox-cub@users.noreply.github.com>
2025-07-23 17:32:51 +0400

    Add files via upload

---
+++ js/articles.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.articles-item').forEach(item => {
+    item.addEventListener('click', function (e) {
+        const isLink = e.target.closest('a');
+        if (!isLink) {
+            window.location.href = this.dataset.href;
+        }
+    });
+});
